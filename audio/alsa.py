bab81a78adf032800673872375fffbc30ff653bc
Olof Fredriksson <fredriksson@olof.nu>
2013-08-11 07:53:31 +0200

    Alsa renamed to pactl, also added som loggings

--- audio/alsa.py
+++
@@ -1,5 +0,0 @@
-import os
-import sys
-class Alsa:
-    def setAudio(self,value):
-        os.system("pactl set-sink-volume 0 -- " + str(value) + "%" )
--- sleep.py
+++ sleep.py
@@ -1,18 +1,24 @@
 # -*- coding: utf-8 -*-
-from audio.alsa import *
+from audio.pactl import *
 from datetime import *
+import logging
 class Sleep:
     def __init__(self, secondsLeft, startUpAudioVolume):
         self.endTime = datetime.now() + timedelta(seconds=secondsLeft)
-
-        #Only have support for Alsa plugin right now, should be possible to add more plugins
-        self.audio = Alsa()
+        self.AudioDisabled = False
+        
+        #Only have support for pactl plugin right now, should be possible to add more plugins
+        self.audio = Pactl()
         self.audio.setAudio(startUpAudioVolume)
 
     def ticker(self):
-        if datetime.now() > self.endTime:
-            self.audio.setAudio(-20)
+        if datetime.now() > self.endTime and self.AudioDisabled == False:
+            self.audio.setAudio(0)
+            logging.info("Audio is disabled")
+            self.AudioDisabled = True
 
     def increaseTicker(self, seconds):
+        self.AudioDisabled = False
         self.endTime = datetime.now() + timedelta(seconds=seconds)
+        logging.info("Audio is activated, end time is" + str(self.endTime))
         self.audio.setAudio(100)
